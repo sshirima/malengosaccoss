e183259dd90d195796b31c9322e93f7260afea23
sshirima <shirimas@gmail.com>
2022-02-04 00:31:56 +0300

    Changing Login URL setting

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #env('DEBUG')
+DEBUG = False #env('DEBUG')
 
 ALLOWED_HOSTS = ['malengosaccoss-test.herokuapp.com']
 
@@ -152,8 +152,8 @@ MESSAGE_TAGS= {
 }
 
 #Loging URL
-LOGIN_URL = '/authentication/login/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/authentication/login'
+LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'authentication.User'
 
