92c59bd74c99c7b78cea8e78850768f16f2fd9d8
sshirima <shirimas@gmail.com>
2022-04-12 23:01:49 +0300

    Updating allowed host settings

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = True #os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #if os.environ.get('DEBUG') == "True" else False
 
-ALLOWED_HOSTS = []#[os.environ.get('SERVER_HOSTNAME')]
+ALLOWED_HOSTS = ['malengosaccos-env-test.eba-2weqvctr.us-west-2.elasticbeanstalk.com']#[os.environ.get('SERVER_HOSTNAME')]
 
 
 # Application definition
